86dbda10834e75cdc0280f8e5aa34c00e779cf7a
Anna <zhironkinanna@gamil.com>
2024-01-25 11:08:41 -0500

    add: slick slider

--- index.php
+++ index.php
@@ -11,6 +11,11 @@ require_once "./functions/main.php";
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/style.css">
+  
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="node_modules/slick-carousel/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="node_modules/slick-carousel/slick/slick-theme.css"/>
+    <script type="text/javascript" src="node_modules/slick-carousel/slick/slick.min.js"></script>
 </head>
 
 <body>
@@ -58,7 +63,7 @@ require_once "./functions/main.php";
         ?>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- pages/uploads.php
+++ pages/uploads.php
@@ -9,8 +9,32 @@
     <button class="btn btn-primary" name="action" value="uploadImage">Send</button>
 </form>
 
-
-<?php
-// $files = glob('./uploaded/*.{jpg,jpeg,png,gif,webp,avif}', GLOB_BRACE);
-// dump($files);
+ <?php
+$files = glob('./uploaded/small/*.{jpg,jpeg,png,gif,webp,avif}', GLOB_BRACE);
+dump($files);
 ?>
+
+
+<div class="slick-fade">
+    <?php
+    foreach ($files as $file) {
+        echo '<img src="' . $file . '" alt="Image">';
+    }
+    ?>
+</div> 
+
+ <script type="text/javascript">
+    $(document).ready(function(){
+        $('.slick-fade').slick({
+            dots: true,
+            infinite: true,
+            speed: 700,
+            autoplay:true,
+            autoplaySpeed: 2000,
+            arrows:false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            variableWidth: true,
+        })
+    });
+</script> 
